278cd13e680167ca579903a567d8ba6442e5e3a3
Guilherme Alves <guilherme.garcia86@gmail.com>
2021-12-03 16:22:16 -0300

    Adicionado fastify

--- index.js
+++ index.js
@@ -1,26 +1,59 @@
-const http = require('http')
-const people = require('./data/people')
+const fastify = require('fastify')({ logger: true })
 
-const server = http.createServer((req, res) => {
-    if(req.url === '/api' && req.method === 'GET'){
-        res.writeHead(200, {'Content-Type': 'application/json'})
-        res.end(JSON.stringify(people))
-    }else if(req.url.match(/\/api\/([0-9]+)/)){
-        const id = req.url.split('/')[2]
-        const person = people.find((p) => p.id == id)
-        if(!person){
-            res.writeHead(404, {'Content-Type': 'application/json'})
-            res.end(JSON.stringify({message: 'Pessoa nao encontrada'}))
-        } else {
-            res.writeHead(200, {'Content-Type': 'application/json'})
-            res.end(JSON.stringify(person))
-        }
-    }else{
-        res.writeHead(400, {'Content-Type': 'application/json'})
-        res.end(JSON.stringify({message: 'Rota nao encontrada'}))
+const cervejas = [
+    {
+        "nome": "Bud",
+        "tipo": "Larger",
+        "quantidade": 10
+    },
+    {
+        "nome": "Skol",
+        "tipo": "Pilsen",
+        "quantidade": 15
+    },
+    {
+        "nome": "Coruja",
+        "tipo": "IPA",
+        "quantidade": 5
     }
+]
+
+fastify.get('/cervejas', async (request, reply) => {
+  return cervejas
 })
 
-server.listen(3000, () => console.log("Server rodando na porta 3000"))
+fastify.get('/cerveja/:nome', async(req, resp) => {
+    const nomeDaCerveja = req.params.nome
 
-module.exports = server
+    const cerveja = cervejas.find(elem => elem.nome === nomeDaCerveja)
+
+    if(!cerveja){
+        const msg = {"message": `Cerveja ${nomeDaCerveja} nao foi encontrada`}
+        resp.code(404).send(msg)
+    }
+
+    return cerveja
+})
+
+fastify.post('/cerveja', async(req, res) => {
+    const cerveja = req.body
+
+    if(cerveja?.nome || cerveja?.tipo || cerveja?.quantidade <= 0){
+        const msg = {msg: "ERRRRROOOOOUUUUU"}
+        res.code(400).send(msg)
+    }
+
+    cervejas.push(cerveja)
+
+    res.code(201).send(cerveja)
+})
+
+const start = async () => {
+  try {
+    await fastify.listen(3000)
+  } catch (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
+}
+start()
