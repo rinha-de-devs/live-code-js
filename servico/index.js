0ffdcd9bb18de30e0d0af9cf02b2b232e938a6d8
Guilherme Alves <guilherme.garcia86@gmail.com>
2021-12-10 11:00:10 -0300

    add ServicoCerveja

--- servico/index.js
+++ servico/index.js
@@ -1,8 +1,42 @@
 const Cerveja = require('../dominio')
 
-class CervejaServico{
+class ServicoCerveja {
 
     constructor(repositorio){
         this.repositorio = repositorio
     }
-} 
+
+    inserirCerveja(cerveja){
+        const cervejaPraSalvar = new Cerveja(Math.floor(Math.random() * 100), cerveja.nome, cerveja.tipo, cerveja.quantidade)
+        this.repositorio.inserir(cervejaPraSalvar)
+        return cervejaPraSalvar.nome
+    }
+
+    buscarCervejas(){
+        return this.repositorio.buscarTodas()
+    }
+
+    buscarCervejaPorId(id){
+        const cerveja = this.repositorio.buscarCervejaPorId(id)
+        if(cerveja){
+            return cerveja
+        }
+        return Error('Erro na busca da cerveja')
+    }
+
+    alterarCerveja(cerveja){
+        const resultado = this.repositorio.alterarCerveja(cerveja)
+        if(resultado){
+            return resultado 
+        }
+        return Error('Erro na exclusão da cerveja')
+    }
+
+    excluirCerveja(id){
+        const resultado = this.repositorio.excluirCerveja(id)
+        if(resultado){
+            return resultado 
+        }
+        return Error('Erro na exclusão da cerveja')
+    }
+}
